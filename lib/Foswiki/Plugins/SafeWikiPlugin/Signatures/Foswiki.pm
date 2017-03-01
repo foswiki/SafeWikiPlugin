f79aab053a3a4ace7bc521556656774619a251cd
George Clark <geonwiki@fenachrone.com>
2017-03-01 13:53:48 -0500

    Item14334:  Add the rest of 2.1.3 signatures
    This is all topics that caused a warning due to an on* handler

--- lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/Foswiki.pm
+++ lib/Foswiki/Plugins/SafeWikiPlugin/Signatures/Foswiki.pm
@@ -17,4 +17,60 @@
 
     # System.BulkRegistration
     'Jv55fvw1BVbO9cycPnMjDYqzIf77ckSdKHaNyZ3865s',    #2.2.0
+
+    # System.PerlDependencyReport
+    'OnLVo2bFksh53WiCWDqNcwbew/BGdSNdP/wYV2AfQv0',
+
+    # System.FormatTokens
+    'dM6DyGVrrIwFD/XwsHFDlgpa1u/Mp0J7j2Ps0390fRg',    #2.1.3
+    'YTnUyZDQp8q4+g4ck/+DkE9TusN+w+KREKwkZ1UQ3Vc',    #2.1.3
+
+    # System.EditTablePlugin
+    '8chQcnZVbNLOxjC2TMr3GVykYyenxecMZlZssnimqyM',    #2.1.3
+
+    # System.VarBUTTON
+    'jVwMZaunrCA5Z1pGipms1leIC30cCTpIr8Cj4Ch7x0Y',    #2.1.3
+
+    # System.JQueryLoader
+    'MG0M8IThIN5S1zgEvJ+kaLj3gWB6xCIVmc+UdKJmiWk',    #2.1.3
+
+    # System.CompleteDocumentation
+    'u0xMW4qZfpuHIChJQJktzzLftXtjC6ZTNIrU+/90Wss',    #2.1.3
+
+    # System.JQueryButton
+    'RitcfmPKW3COIQ89JP1IQXBQvoKAeML7Nd28Nf87pp0',    #2.1.3
+    'zRv6MG518cGe+OgeXt3faED8LlL7jeovaK/wwJvfvKI',    #2.1.3
+
+    # System.JQueryPNotify
+    'MD6TPLdeAD7fxxnfHe/qnit1hvrfZCUO8iJYs3TzdDc',    #2.1.3
+    '1/fnrf2Ct1lQykiPUJd4CKlzzbgrsXC3yiSLLhAczdc',    #2.1.3
+
+    # System.JQueryShake
+    'RiKJDy9C/fo1Y+hgNlB3hknPz1sZshE201AlJ2GZs6c',    #2.1.3
+
+    # System.JSCalendarContrib
+    'dwnDoBBeS+fAO2GnraWacsZpxyP90zIroMdAkTE+zws',    #2.1.3
+
+    # System.BulkResetPassword
+    'SEACisicFxKlIZhwX0U6nRqCazyiIiBu7NpCXLs9NlI',    #2.1.3
+
+    # System.PatternSkinElements
+    'UCFwKTmXj0qx2u/etz0CYXVS5fY2ESFdsi2Y0bYWEPo',    #2.1.3A
+
+    # System.SearchPatternCookbook
+    'PXzeD02r1elnh7JwrdCusYvbPwrOM5zZjHec0vn+yYs',    #2.1.3
+    'KJO5O/teRK7Tsk1RNIBXbUXgce0/pigXenVbdhdZVK0',
+    'LmDoNr5fMZOzNaB3Gaq0CvlFzGZEyuGTTGf89vF0EuQ',
+    'eKmIolTmNfCGO2B0GdNxBz0r1n/A0wv7sz8brd1iG2E',
+    'VDnju+GodmbFzj/dDxaiiWX7n4bSTbaBTkmlsYKvH5Q',
+    'yottXzavqORy8naBfyNSg9bGHDvBfalyXJ4RUz1TmgY',
+    'q7lVVjRcmIHdPablI/ClnY+gSGblw9r/vBqWOisUwQo',
+    'y2c/CAgq+7Bnzunhf6d6oscOk9ygLD0gCApRQ5kmDZU',
+    'yZMah+nye+wDkzT3DfE/ZZ/d8MBdOqGF8sS7JywQKts',
+    '8ejfWJnY/HdAGAIbs5g0IuIBLXeqbGKoyrOwjTF+jMc',
+    'qm611oTwYdiA437Wn604oAk7vY/VdTb0C+YkzhqPMNY',
+    'EHuyDx89QPcsiGB+xUVhwj8PjI5yz7nTVwyioR5mjTo',
+
+    # System.SiteChanges
+    'Gm6s2a9Fo8jqLrdH+zM4iH6/6IJ/OUWevTs7tuVINL8',    #2.1.3
 ];
